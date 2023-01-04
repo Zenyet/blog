8e00c3d0fee413076db118f81e77f499474930ce
Zenyet <zenyet1998@gmail.com>
2023-01-04 17:19:11 +0800

    initial commit

---
+++ assets/tags.eae548a3.js
@@ -0,0 +1,1 @@
+import{m as t}from"./index.246b2081.js";function r(){return t({method:"GET",url:"/api/tags"})}function e(a){return t({method:"PUT",url:"/api/tags/taxonomy",data:{tag_name:a}})}export{e as a,r as g};
